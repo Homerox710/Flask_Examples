bc373673b64ea26995232277e0d07e9f954f456b
Elias Mena <eliasmena.2911@gmail.com>
2021-06-20 23:16:24 -0600

    Guardar una sesion

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video']
 
@@ -20,12 +22,12 @@ def internal_server_error(error):
 def index():
     user_ip = request.remote_addr #Vamos a guardar la ip del usuario en una cookie y devolvemos una respuesta de flask
     response = make_response(redirect('/hello')) #Redirigimos la respuesta a /hello
-    response.set_cookie('user_ip',user_ip)
+    session['user_ip'] = user_ip #Solicitamos la ip por session para encriptar el dato
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip') #Obtenemos la cookie que seteamos en index
+    user_ip = session.get('user_ip') #Obtenemos la ip de session
     context = {
         'user_ip' : user_ip,
         'todos':todos
