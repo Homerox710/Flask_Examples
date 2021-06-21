a195a2e133ec59f0047f95c5ff70459a04184784
Elias Mena <eliasmena.2911@gmail.com>
2021-06-20 23:34:27 -0600

    Implementar bootstrap and wtf

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -7,6 +10,14 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+#Validador de datos
+
 
 # Manejar error 404 (not found)
 @app.errorhandler(404)
@@ -28,9 +39,11 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip') #Obtenemos la ip de session
+    login_form = LoginForm()
     context = {
         'user_ip' : user_ip,
-        'todos':todos
+        'todos':todos,
+        'login_form':login_form
     }
     return render_template('hello.html', **context) #Diccionario expandido para referenciarlas directamente
 
