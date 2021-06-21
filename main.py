4ec62043c39b903dfcf4aeeb6b1acafe1ca4b64f
Elias Mena <eliasmena.2911@gmail.com>
2021-06-20 20:23:05 -0600

    Socitar IP al usuario

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
- 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    user_ip = request.remote_addr
+    return f'Hello World Flask, tu IP es {user_ip}'
+
