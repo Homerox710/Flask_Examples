991ea1c990cc30fe5898977cc5bfba57c8de7188
Elias Mena <eliasmena.2911@gmail.com>
2021-06-20 22:38:18 -0600

    Manejo de páginas de error

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ app = Flask(__name__)
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video']
 
+# Manejar error 404 (not found)
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html',error=error) #Retornamos el html y una variable con la info del error
+
+# Manejar error 500 (server)
+@app.errorhandler(500)
+def internal_server_error(error):
+  return render_template('500.html')
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr #Vamos a guardar la ip del usuario en una cookie y devolvemos una respuesta de flask
