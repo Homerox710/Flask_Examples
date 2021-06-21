1bf8c1361f87188fc741df2f4f74ac8e213dd87b
Elias Mena <eliasmena.2911@gmail.com>
2021-06-21 02:24:35 -0600

    TESTS

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -16,7 +17,11 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     submit = SubmitField('Enviar')
 
-#Validador de datos
+#Comando para testing
+@app.cli.command()
+def test():
+    test = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner().run(test)
 
 
 # Manejar error 404 (not found)
