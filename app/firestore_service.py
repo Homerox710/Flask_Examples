400895228aaa625a690dcf19510038029deee33f
Elias Mena <eliasmena.2911@gmail.com>
2021-07-04 03:22:56 -0600

    finish

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -22,3 +22,18 @@ def get_todos(user_id):
 def user_put(user_data):
     user_ref = db.collection('users').document(user_data.username)
     user_ref.set({'password': user_data.password})
+
+def put_todo(user_id, description):
+    todos_collection_ref = db.collection('users').document(user_id).collection('todos')
+    todos_collection_ref.add({'description': description, 'done':False})
+
+def delete_todo(user_id, todo_id):
+    todo_ref = _get_todo_ref(user_id,todo_id)
+    todo_ref.delete()
+
+def update_todo(user_id, todo_id, done):
+    todo_ref = _get_todo_ref(user_id,todo_id)
+    todo_ref.update({'done': not done})
+
+def _get_todo_ref(user_id, todo_id):
+    return db.collection('users').document(user_id).collection('todos').document(todo_id)
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from flask_login import login_required, current_user
 
 
 from app import create_app
-from app.forms import TodoForm
-from app.firestore_service import get_users, get_todos, put_todo
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+from app.firestore_service import get_users, get_todos, put_todo, delete_todo, update_todo
 app = create_app()
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video']
@@ -37,15 +37,40 @@ def index():
     return response
 
 #Especificamos los métodos permitidos
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET','POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip') #Obtenemos la ip de session
     username = current_user.id
+    todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
     context = {
         'user_ip' : user_ip,
         'todos': get_todos(user_id=username),
-        'username': username
+        'username': username,
+        'todo_form':todo_form,
+        'delete_form': delete_form,
+        'update_form':update_form
     }
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
+
+        flash('Tu tarea se creó con éxito')
+
+        return redirect(url_for('hello'))
 
     return render_template('hello.html', **context) #Diccionario expandido para referenciarlas directamente
+
+#Ruta dinámica
+@app.route('/todos/delete/<todo_id>',methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id=user_id, todo_id=todo_id)
+    return redirect(url_for('hello'))
+
+@app.route('/todos/delete/<todo_id>/<int:done>',methods=['POST'])
+def update(todo_id,done):
+    user_id = current_user.id
+    update_todo(user_id=user_id, todo_id=todo_id, done=done)
+    return redirect(url_for('hello'))
